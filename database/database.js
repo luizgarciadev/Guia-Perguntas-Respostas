78024d8534483e8c4bbf218c177117a9b88aee15
Luiz Garcia <luizgarcia231@gmail.com>
2021-04-05 16:01:52 -0300

    Terceiro Commit

--- database/database.js
+++ database/database.js
@@ -1,8 +1,7 @@
 const Sequelize = require('sequelize'); //biblioteca para conectar o JS ao banco de dados MySql
 
-const connection = new Sequelize('guiaperguntas','root','123456789',{
+const connection = new Sequelize('guiaperguntas','root','123456',{
     host: 'localhost',
     dialect:'mysql'
 });
-
 module.exports = connection; //exportar modulo database
